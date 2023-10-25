e8f7c9c0a691ee030c71c5de24df0b960408070c
Amen allah Jouini <amenallah.jouini@esprit.tn>
2023-10-25 21:58:26 +0100

    cors

--- src/app/service/org.service.ts
+++ src/app/service/org.service.ts
@@ -8,32 +8,32 @@ import { Org } from '../Models/org';
 })
 export class OrgService {
   private baseUrl = 'http://localhost:8044'; // Remplacez par l'URL de votre backend
-  private base = 'http://localhost:8090/MICROORGANISATION-S/'; // Remplacez par l'URL de votre backend
+  private base = 'http://localhost:8090/MICROORGANISATION-S'; // Remplacez par l'URL de votre backend
 
   constructor(private http: HttpClient) {}
 
   // Méthode pour récupérer tous les articles
   getAllOrgs(): Observable<Org[]> {
-    return this.http.get<Org[]>(`${this.base}/`);
+    return this.http.get<Org[]>('http://localhost:8090/MICROORGANISATION-S');
   }
 
   // Méthode pour ajouter un nouvel article
   addOrg(org: Org): Observable<Org> {
-    return this.http.post<Org>(`${this.baseUrl}/`, org);
+    return this.http.post<Org>('http://localhost:8044', org);
   }
 
   // Méthode pour mettre à jour un article
   updateOrg(id: number, org: Org): Observable<Org> {
-    return this.http.put<Org>(`${this.baseUrl}/${id}`, org);
+    return this.http.put<Org>(`http://localhost:8044/${id}`, org);
   }
 
   // Méthode pour supprimer un article
   deleteOrg(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+    return this.http.delete<void>(`http://localhost:8044/${id}`);
   }
 
   // Méthode pour obtenir un article par ID
   getOrgById(id: number): Observable<Org> {
-    return this.http.get<Org>(`${this.baseUrl}/${id}`);
+    return this.http.get<Org>(`http://localhost:8044/${id}`);
   }
 }
