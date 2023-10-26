4556964e3bb42208d6fd5c06d1d93b45856dc4f2
Elyes Boudhina <elyes.boudhina@etudiant-fsegt.utm.tn>
2023-10-26 13:27:41 +0100

    feat: adding user service

---
+++ src/app/Service/user.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Post} from "../Models/post.model";
+import {User} from "../Models/user.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private fetchAllUsersURL = 'http://localhost:8075/user/users';
+  private fetchUserByIdURL = 'http://localhost:8075/user/';
+  private deleteUsersByIdURL = 'http://localhost:8075/user';
+  private addUserURL = 'http://localhost:8075/user/add';
+  constructor(private http: HttpClient) {
+
+  }
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.fetchAllUsersURL);
+  }
+  deleteUser(id: number): Observable<any> {
+    const deleteUrl = `${this.deleteUsersByIdURL}/${id}`;
+    return this.http.delete<any>(deleteUrl, { responseType: 'json'});
+  }
+
+  getUserById(id: number): Observable<any> {
+    const fetchUserUrl = `${this.fetchUserByIdURL}/${id}`;
+    return this.http.get<any>(fetchUserUrl, { responseType: 'json'});
+  }
+  createUser(id: number, userData: any) :Observable<any> {
+    console.log("before");
+    return this.http.post<any>(`${this.addUserURL}/${id}`, userData);
+
+  }
+
+
+  }
