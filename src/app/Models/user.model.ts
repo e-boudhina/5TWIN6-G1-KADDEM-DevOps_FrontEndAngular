2c629fe88df0ac940ee5fc3a2f57737c51b414e5
Elyes Boudhina <elyes.boudhina@etudiant-fsegt.utm.tn>
2023-10-26 13:31:37 +0100

    feat: changing user fields to lowercase

--- src/app/Models/user.model.ts
+++ src/app/Models/user.model.ts
@@ -1,9 +1,9 @@
 export class User {
   id: number;
   username: string;
-  firstName: string;
-  lastName: string;
-  phoneNumber: string;
+  firstname: string;
+  lastname: string;
+  phonenumber: string;
   address: string;
   email: string;
   age: number;
